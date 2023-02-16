ff772066d5ff91d21067468c1dc6b8038c35a0f2
Sounak Saha <sounaksaha999@gamil.com>
2023-02-16 21:03:49 +0530

    all files uploaded

---
+++ 3. PW - JS/13th_Feb/yes.js
@@ -0,0 +1,60 @@
+// Values
+// Values
+
+// 7 => int
+// 8.1 => Float
+// Number
+
+// 'Anurag'
+// "Anurag"
+// String
+
+// true or false
+// Boolean values
+
+// Null
+// undefined
+
+// Prmitive Datatype
+// Single value
+
+// Non - Primitive values => Multi values
+// array => [] => index
+// Object => {} => Key:Value
+
+// ["A", "B", "C", "D", "E", "F","G"];
+// {}
+
+// Variables
+
+// var
+// let
+// const
+
+// let number = 2510;
+
+// let number2 = 34;
+// console.log(number);
+
+// console.log(number2);
+
+// Assignment Operator
+let a = 50;
+let b = 25;
+let c = 50;
+
+// Arithmetic Operator
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// console.log(3 ** 4);
+
+// Comparision OP
+
+// ==
+// ===
+console.log(a >= c);
---
+++ 3. PW - JS/14th_Feb/script.js
@@ -0,0 +1,69 @@
+// Variable and Values (Number, String Boolean etc)
+// Operators
+
+// Conditions and Loops
+
+let age = 17;
+
+// if (age >= 18) {
+//   console.log("You are Allowed");
+// } else {
+//   console.log("You are not allowed");
+// }
+
+// let signal = "green";
+// if (signal == "red") {
+//   console.log("Red => STOP");
+// } // Condtion 1
+// else if (signal == "yellow") {
+//   console.log("Yellow => Go Slow");
+// } // Conditon 2
+// else if (signal == "green") {
+//   console.log("Green => Go Fast");
+// } else {
+//   console.log("Invalid");
+// } // Last
+
+// Switch Case
+// let user = "Admin";
+// switch (user) {
+//   case "Admin":
+//     console.log("He is Admin");
+//     break;
+
+//   case "Student":
+//     console.log("He is Student");
+//     break;
+
+//   case "Mentor":
+//     console.log("He is Mentor");
+//     break;
+//   default:
+//     console.log("I am Default");
+// }
+
+// Loops
+// do while, while, for
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// while (init, Conditon, inc/dec)
+
+// let i = 0;
+// while (i > 5) {
+//   console.log(i);
+//   i++;
+// }
+// let i = 0;
+// do {
+//   console.log("Hello World");
+//   i++;
+// } while (i < 5);
+
+// Ternary Op / Ternay Conditon
+//  condtion ? TRUE:False
+
+isloggedin = false;
+isloggedin ? console.log("Buying Mer") : console.log("Not Buying");
